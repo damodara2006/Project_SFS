3c3ab6f649f789214d5a3e2e2ec38c902285df09
badri-2005 <badrinarayananramkumar@gmail.com>
2025-10-29 20:44:28 +0530

    feat : added Login Component in app.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+        <form action="">
+            <label htmlFor="Role">Select Your Role</label>
+            <select name="role" id="role">
+                <option value="Admin">Admin - SACL</option>
+                <option value="Evaluator">Evaluator - SACL</option>
+                <option value="spoc">SPOC</option>
+                <option value="student">Student</option>
+            </select>
+            <br />
+            <label htmlFor="Email">Email</label>
+            <input type="email" />
+            <br />
+            <label htmlFor="password">Password</label>
+            <input type="password" />
+            
+            <button>Submit</button>
+        </form>
+    </div>
   )
 }
 
