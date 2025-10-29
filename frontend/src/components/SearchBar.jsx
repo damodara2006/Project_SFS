801191d7ca26164e541cb7d2f9b02b821665c8dd
Hari Dev <hariharidev5@gmail.com>
2025-10-29 23:07:57 +0530

    refactor: Improve Search component functionality

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,19 +3,24 @@ import {useState} from "react";
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
+  // Function to handle input change
+  const onChange = () => (e) => {
+    setSearchTerm(e.target.value);
+  }
+  // Function to handle search input at backend
   const handleSearch =(e)=>{
-    e.preventDefault();
     // Implement search functionality here
     console.log("Searching for:", searchTerm);
   }
+
   // JSX for Search Bar
-  
   return (
     <>
       <div className="search-bar flex items-center justify-center gap-5 w-[30 rem]">
-        <input 
-        type="text" placeholder="Search..." 
-        className="w-88 h-10 focus:outline-gray-200 bg-white backdrop-blur-xl p-2 rounded-3xl shadow-lg " />
+        <input
+          type="text" placeholder="Search..."
+          className="w-88 h-10 focus:outline-gray-200 bg-white backdrop-blur-xl p-2 rounded-3xl shadow-lg "
+          onChange={onChange()} />
         <button type="submit" className="bg-[#fc8f00] text-white font-semibold py-2 rounded-md mx-4 p-4" onClick={handleSearch}>Search</button>
       </div>
     </>
