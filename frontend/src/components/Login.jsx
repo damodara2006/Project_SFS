6189201c299930909818d309798b8197fd9295ce
badri-2005 <badrinarayananramkumar@gmail.com>
2025-10-29 21:26:01 +0530

    feat : updated login.jsx page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
-
 const Login = () => {
   return (
     <div>
         <form action="">
             <label htmlFor="Role">Select Your Role</label>
+
             <select name="role" id="role">
                 <option value="Admin">Admin - SACL</option>
                 <option value="Evaluator">Evaluator - SACL</option>
                 <option value="spoc">SPOC</option>
                 <option value="student">Student</option>
             </select>
+
             <br />
+
             <label htmlFor="Email">Email</label>
             <input type="email" />
+
             <br />
+
             <label htmlFor="password">Password</label>
             <input type="password" />
-            
-            <button>Submit</button>
+
+            <br />
+
+            <a href="" className='text-blue-600'>Forgot Your Password?</a>
+
+            <br />
+
+            <button className='bg-amber-500 p-2 rounded-2xl font-bold text-white'>Submit</button>
         </form>
     </div>
   )
