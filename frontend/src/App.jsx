4e90960cb75b7e41a0b0565a44003858bebf63f8
Hari Dev <hariharidev5@gmail.com>
2025-10-29 22:45:17 +0530

    refactor: update Home route to render SearchBar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import Login from "./components/Login"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Register from "./components/Register"
+import SearchBar from "./components/SearchBar"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<SearchBar/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />}/>
       </Routes>
