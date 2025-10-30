2bf67e1b84049d832a2c177e071d7d9ba31a5429
damodara2006 <damodara2006@gmail.com>
2025-10-30 19:41:59 +0530

    feat: implement StudentNav toggle functionality and add Student_submissions component

--- frontend/src/pages/student/SdDashboard.jsx
+++ frontend/src/pages/student/SdDashboard.jsx
@@ -2,14 +2,20 @@ import StudentNav from "../../components/StudentNav";
 import TeamDetails from "./TeamDetails";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { FaGripLines } from "react-icons/fa";
+import { useState } from "react";
 
 const SdDashboard = () => {
+    const[shownav, setshownav] = useState(false)
     return (
-        <div >
+        <div  className="min-w-screen min-h-screen border-4">
             <Header />
-            <StudentNav/>
-            <TeamDetails/>
-            <Footer />
+            
+            {
+                shownav ? <div className="bg-orange-400  px-2  w-fit absolute z-30 top-21 right-0"><StudentNav />  </div> : ""
+            }
+            <div className="pt-22 relative"><TeamDetails /> <span className="absolute top-7 right-10 text-white text-2xl" onClick={()=>setshownav(!shownav)}><FaGripLines /></span></div>
+            <div className="bottom-0 w-full fixed"> <Footer /></div>
         </div>
     )
 }
