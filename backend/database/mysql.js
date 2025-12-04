0a9ae31be057e9b919a7606da07d6a6d88c9a47a
damodara2006 <damodara2006@gmail.com>
2025-12-04 14:05:46 +0530

    fix: update database host and correct password variable in MySQL connection

--- backend/database/mysql.js
+++ backend/database/mysql.js
@@ -1,10 +1,12 @@
 import { createPool } from "mysql2/promise"
+import dotenv from "dotenv"
 
+dotenv.config()
 const connection = await createPool({
     host: process.env.DB_HOST || "54.234.143.213",
     database: process.env.DB_NAME || "Sakthi_auto",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "root@1234",
+    password: process.env.DB_PASSWORD || "Damo@999428",
     waitForConnections: true,
     connectionLimit: parseInt(process.env.DB_CONN_LIMIT || '10', 10),
     queueLimit: parseInt(process.env.DB_QUEUE_LIMIT || '10', 10),
